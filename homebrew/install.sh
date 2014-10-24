44b207cf5e586e4993ace9f5f1dcb042b6cdadd4
rex <r030515495@gmail.com>
2014-10-24 15:26:02 +0800

    My first dot files push

---
+++ homebrew/install.sh
@@ -0,0 +1,1 @@
+brew install ant coreutils nginx tomcat tomcat-native jetty maven solr mysql node imagemagick python caskroom/cask/brew-cask
---
+++ sublimeText/setting.sh
@@ -0,0 +1,27 @@
+# setting Sublime3
+cd ~/Library/Application\ Support/Sublime\ Text\ 3/
+rm -rf "Installed Packages"
+rm -rf "Packages"
+
+ln -s  ~/Dropbox/06NoteBookBK/Sublime3/Packages
+ln -s  ~/Dropbox/06NoteBookBK/Sublime3/Installed\ Packages
+
+#link
+sudo rm -rf /bin/subl
+sudo ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /bin/subl
+
+# setting Sublime2
+cd ~/Library/Application\ Support/Sublime\ Text\ 2
+
+rm -rf "Installed Packages"
+rm -rf "Pristine Packages"
+rm -rf "Packages"
+
+ln -s ~/Dropbox/06NoteBookBK/Sublime2/Installed\ Packages
+ln -s ~/Dropbox/06NoteBookBK/Sublime2/Pristine\ Packages
+ln -s ~/Dropbox/06NoteBookBK/Sublime2/Packages
+
+
+
+
+
