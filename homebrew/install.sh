a9f142b95e23b894752edaae137845b618955e0d
RexLi <r030515495@gmail.com>
2014-10-27 15:05:43 +0800

    1.add application 2.backup Stickies

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -4,7 +4,13 @@ brew install coreutils nginx mysql solr node imagemagick python tree caskroom/ca
 brew cask install cord diskwave dropbox genymotion \
 google-chrome iterm2 java keka macdown magican \
 mplayerx skitch skype sourcetree sublime-text \
-thunder virtualbox welly firefox simple-comic
+thunder virtualbox welly firefox simple-comic \
+jd-gui spotify
 
 #need java
-brew install ant tomcat tomcat-native jetty maven
+brew install ant tomcat tomcat-native jetty maveca
+
+
+## no cask application
+# sublime text3
+
--- osx/install.sh
+++ osx/install.sh
@@ -1,3 +1,7 @@
+#Remote Server conect setting
 rm -rf ~/Library/Application\ Support/CoRD/Servers
 ln -s ~/Dropbox/06NoteBookBK/CoRD/Servers ~/Library/Application\ Support/CoRD/Servers
+# base bash
 ln -s  /Users/rex/Dropbox/06NoteBookBK/shell/.bash_profile .bash_profile
+#Mac Stickies
+ln -s /Users/rex/Dropbox/06NoteBookBK/MacStickies/StickiesDatabase ~/Library/StickiesDatabase
