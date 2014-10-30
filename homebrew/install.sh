aae53c7cbc83e7a723f4a7521d1fe2a83a43d36e
RexLi <r030515495@gmail.com>
2014-10-30 11:57:23 +0800

    change

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,11 +1,13 @@
 #base
-brew install coreutils nginx mysql solr node imagemagick python tree caskroom/cask/brew-cask
+brew install coreutils nginx mysql solr node imagemagick python tree ntfs-free caskroom/cask/brew-cask vagrant
 #add
-brew cask install cord diskwave dropbox genymotion \
-google-chrome iterm2 java keka macdown magican \
-mplayerx skitch skype sourcetree sublime-text \
-thunder virtualbox welly firefox simple-comic \
-jd-gui spotify
+brew cask install \
+cord google-chrome macdown skype virtualbox \
+diskwave iterm2 magican sourcetree welly \
+dropbox java mplayerx spotify \
+firefox jd-gui simple-comic sublime-text \
+genymotion keka skitch thunder ffmpeg
+
 
 #need java
 brew install ant tomcat tomcat-native jetty maveca homebrew/versions/tomcat7
