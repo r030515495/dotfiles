b13478103a2b392d85958bb657d805fa9d250236
RexLi <r030515495@gmail.com>
2014-11-17 23:36:44 +0800

    update dutiful

---
+++ brew.sh
@@ -0,0 +1,66 @@
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#base
+brew install node
+brew install python
+brew install tree
+brew install coreutils
+brew install nginx
+brew install imagemagick
+
+brew install caskroom/cask/brew-cask
+brew install vagrant
+brew install ffmpeg
+
+#cask
+brew cask install cord
+brew cask install diskwave
+brew cask install dropbox
+brew cask install firefox
+brew cask install genymotion
+brew cask install google-chrome
+brew cask install iterm2
+brew cask install java
+brew cask install jd-gui
+brew cask install keka
+brew cask install macdown
+brew cask install magican
+brew cask install mplayerx
+brew cask install simple-comic
+brew cask install skitch
+brew cask install skype
+brew cask install sourcetree
+brew cask install spotify
+brew cask install sublime-text
+brew cask install the-unarchiver
+brew cask install thunder
+brew cask install vagrant
+brew cask install virtualbox
+brew cask install welly
+
+
+#need java
+brew install ant
+brew install tomcat-native
+brew install jetty
+brew install maven
+brew install homebrew/versions/tomcat7
+
+
+## no cask application
+
+
+# iFunBox.app/
+# Sublime Text.app/
+# MacMoney.app/
+# iStat Menus.app/
+# Alfred 2.app/
+# MEGAsync.app/
+# Haozhaopian.app/
+# VirtualBox.app/
+# LINE.app/
+# Evernote.app/
+# Magican.app/
+# Xcode.app/
+# Pocket.app/
+# Moom.app/
+# Dash.app/
---
+++ vimrc
@@ -0,0 +1,24 @@
+" Enable line numbers
+set number
+" Enable syntax highlighting
+syntax on
+" Highlight current line
+set cursorline
+" Make tabs as wide as two spaces
+set tabstop=2
+" Highlight searches
+set hlsearch
+" Ignore case of searches
+set ignorecase
+" Highlight dynamically as pattern is typed
+set incsearch
+" Always show status line
+set laststatus=2
+" Enable mouse in all modes
+set mouse=a
+" Show the cursor position
+set ruler
+" Show the current mode
+set showmode
+" Show the filename in the window titlebar
+set title
