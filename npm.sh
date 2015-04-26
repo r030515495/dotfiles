838f9d5c4c1ca4dc2e286fd548c663e3f0b688b6
RexLi <r030515495@gmail.com>
2015-04-26 22:45:02 +0800

    change the npm install base react sample

--- npm.sh
+++ npm.sh
@@ -7,7 +7,7 @@ npm install -g trash
 npm install -g grunt-cli
 npm install -g yo
 npm install -g generator-webapp
-npm install -g generator-react-gulp-browserify
+npm install -g generator-react-boilerplate
 npm install -g generator-angular
 npm install -g gulp
 npm install -g webpack
