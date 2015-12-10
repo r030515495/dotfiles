0a95a1616fb7edbf2e6289476596dbbe1f289d2b
RexLi <r030515495@gmail.com>
2015-12-10 15:18:57 +0800

    註解 sublime 2 設定

--- sublimeProfileLink.sh
+++ sublimeProfileLink.sh
@@ -11,15 +11,15 @@ ln -s  ~/Dropbox/06NoteBookBK/Sublime3/Installed\ Packages
 #sudo ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /bin/subl
 
 # setting Sublime2
-cd ~/Library/Application\ Support/Sublime\ Text\ 2
+# cd ~/Library/Application\ Support/Sublime\ Text\ 2
 
-rm -rf "Installed Packages"
-rm -rf "Pristine Packages"
-rm -rf "Packages"
+# rm -rf "Installed Packages"
+# rm -rf "Pristine Packages"
+# rm -rf "Packages"
 
-ln -s ~/Dropbox/06NoteBookBK/Sublime2/Installed\ Packages
-ln -s ~/Dropbox/06NoteBookBK/Sublime2/Pristine\ Packages
-ln -s ~/Dropbox/06NoteBookBK/Sublime2/Packages
+# ln -s ~/Dropbox/06NoteBookBK/Sublime2/Installed\ Packages
+# ln -s ~/Dropbox/06NoteBookBK/Sublime2/Pristine\ Packages
+# ln -s ~/Dropbox/06NoteBookBK/Sublime2/Packages
 
 
 
